a5d06af2065c74a860d6301da322aca15510c87a
k2core <7179062@gmail.com>
2023-12-23 17:30:40 +0900

    완성
    Changes to be committed:
    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">App.js</div>;
+  const [time, updateTime] = useState(new Date());
+  useEffect(() => {
+    // timer updation logic
+    const timer = setInterval(() => {
+      updateTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="elementcontainer">
+        <h1>Digital Clock</h1>
+        <div className="timeparent">
+          <div className="timecontainer">
+            {/* 문자열을 예쁘게 인쇄해 보세요 */}
+            <span className="time">{time.toLocaleTimeString()}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
